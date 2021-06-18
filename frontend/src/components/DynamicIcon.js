45ca8a21efd0812105c721bccb93a2996f543918
ole-na <82842056+ole-na@users.noreply.github.com>
2021-06-18 10:27:29 +0200

    reduced code

--- frontend/src/components/DynamicIcon.js
+++ frontend/src/components/DynamicIcon.js
@@ -7,24 +7,20 @@ import LocationOnIcon from "@material-ui/icons/LocationOn";
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 
 export default function DynamicIcon({type}) {
-    return(<>
-        {(() => {
-            switch (type) {
-                case 'settings':
-                    return <SettingsIcon color='primary' aria-hidden='true' />;
-                case 'map':
-                    return <LocationOnIcon color="primary" aria-hidden="true" />;
-                case 'warnings':
-                    return <SettingsInputAntennaIcon color="primary" aria-hidden="true" />;
-                case 'help':
-                    return <HelpIcon color="primary" aria-hidden="true" />;
-                case 'about':
-                    return <InfoIcon color="primary" aria-hidden="true" />;
-                case 'upload':
-                    return <AddAPhotoIcon color="primary" aria-hidden="true" />;
-                default:
-                    return null;
-            }
-        })()}
-    </>);
+    switch (type) {
+        case 'settings':
+            return <SettingsIcon color='primary' aria-hidden='true'/>;
+        case 'map':
+            return <LocationOnIcon color="primary" aria-hidden="true"/>;
+        case 'warnings':
+            return <SettingsInputAntennaIcon color="primary" aria-hidden="true"/>;
+        case 'help':
+            return <HelpIcon color="primary" aria-hidden="true"/>;
+        case 'about':
+            return <InfoIcon color="primary" aria-hidden="true"/>;
+        case 'upload':
+            return <AddAPhotoIcon color="primary" aria-hidden="true"/>;
+        default:
+            return null;
+    }
 }
