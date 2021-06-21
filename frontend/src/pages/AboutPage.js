6603735c358022d293e3a8a3043254216ee99ce6
ole-na <82842056+ole-na@users.noreply.github.com>
2021-06-21 09:38:09 +0200

    changed text on About page

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -4,8 +4,12 @@ export default function AboutPage() {
     return (
         <section>
             <h2>About me</h2>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
+            <p>
+                The conversion functions, which are provided in this app, have been created with the most possible precision and care. However, I exclude ourselves from any liability for the correctness of the result.
+            </p>
+            <p>
+                I cannot guarantee the functionality of the web app, in particular using it for navigation purposes is at your own risk. In any case, I recommend an intensive test of the accuracy of the representation and the map material used.
+            </p>
         </section>
     );
 }
