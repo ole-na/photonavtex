bfac3c63e4ee4d1e35b03f76a0bb0e8f2d5b46d8
ole-na <82842056+ole-na@users.noreply.github.com>
2021-06-21 09:46:00 +0200

    added help functions for map functionality and some JS react tests

---
+++ frontend/src/components/map/hooks/geolocationConvertSpeed.js
@@ -0,0 +1,29 @@
+/**
+ * Convert speed in metres per second [m/s] to knots(nautical miles per hour) [kn]
+ *
+ * @returns {number}
+ * @throws {InvalidArgumentException}
+ */
+export default function convertMetersPerSecondToKnots(speed) {
+    try {
+        switch (speed) {
+            case "":
+                throw new Error("Speed is empty");
+                break;
+            case (null || undefined):
+                throw new Error("Speed is null if the implementation is not able to measure it");
+                break;
+            case (isNaN(speed) || typeof speed !== "number"):
+                throw new Error("Speed is not a number");
+                break;
+            default:
+                break;
+        }
+    } catch(err) {
+        console.error("Error: param for speed value is not available or correct");
+    } finally {
+        const approximately = 1.9438;
+        const knotsResult = speed * approximately;
+        return knotsResult;
+    }
+}
---
+++ frontend/src/components/map/hooks/geolocationDistance.js
@@ -0,0 +1,48 @@
+/**
+ * Calculate distance between warning position and current location with Haversine formula
+ *
+ * Quelle: https://www.geodatasource.com/developers/javascript
+ * The sample code is licensed under LGPLv3.
+ *
+ * This routine calculates the distance between two points
+ * (given the latitude/longitude of those points)
+ * using GeoDataSource (TM) prodducts.
+ *
+ * Definitions:
+ *      South latitudes are negative, east longitudes are positive
+ *
+ * Passed to function:
+ *      lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)
+ *      lat2, lon2 = Latitude and Longitude of
+ *      where:  'M' is statute miles (default)
+ *              'K' is kilometers
+ *              'N' is nautical miles
+ *
+ * @param {number} lat1: warning's latitude
+ * @param {number} lon1: warning's longitude
+ * @param {number} lat2: current position's latitude
+ * @param {number} lon2: current position's longitude
+ * @returns {number} spherical distance (shortest distance between two points) in nautical miles
+ */
+export default function geolocationDistance(lat1, lon1, lat2, lon2) {
+    const unit = "N";
+    if ((lat1 === lat2) && (lon1 === lon2)) {
+        return 0;
+    }
+    else {
+        const radLat1 = Math.PI * lat1/180;
+        const radLat2 = Math.PI * lat2/180;
+        const theta = lon1-lon2;
+        const radTheta = Math.PI * theta/180;
+        let dist = Math.sin(radLat1) * Math.sin(radLat2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radTheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit === "K") { dist = dist * 1.609344 }
+        if (unit === "N") { dist = dist * 0.8684 }
+        return dist;
+    }
+}
