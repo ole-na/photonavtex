386372370e68e2bc6c8c89c7c8720a34420835ce
ole-na <82842056+ole-na@users.noreply.github.com>
2021-06-25 23:09:16 +0200

    changed icon for current new warning

--- frontend/src/components/map/children/CurrentMarker.js
+++ frontend/src/components/map/children/CurrentMarker.js
@@ -1,40 +1,26 @@
 import React, {useEffect, useState} from "react";
 import {Marker, Popup} from "react-leaflet";
-import geolocationCurrentPosition from "../hooks/geolocationCurrentPosition";
-import convertMetersPerSecondToKnots from "../hooks/geolocationConvertSpeed";
 import MarkerIcon from "../icons/MarkerIcon";
 
 export default function CurrentMarker() {
     const [state, setState] = useState({
-        hasLocation: false,
         coords: [{
             lat: 54.291,
             long: 11.822,
         }],
-        text: "Error: unable to determine location",
-        url: "",
     });
 
     useEffect(() => {
-        const currentPosition = geolocationCurrentPosition();
-        if(currentPosition) {
-            setState(currentPosition);
-        }
+        // setState();
     }, []);
 
-    const currentMarker = state.hasLocation ? (
-        <Marker position={state.coords} icon={MarkerIcon("current")}>
+    return <Marker position={state.coords} icon={MarkerIcon("current")}>
             <Popup>
-                <h3>You are here</h3>
+                <h3>New Warning</h3>
                 <p>{state.text}</p>
-                ({state.coords} && {state.coords.speed} &&
-                    <p>
-                        Speed: {convertMetersPerSecondToKnots(state.coords.speed)} nm
-                    </p>
-                )
+                <p>
+                    Coords: {state.coords}
+                </p>
             </Popup>
         </Marker>
-    ) : null;
-
-    return currentMarker;
 }
