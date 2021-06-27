fff7ba345f90ddad75e4629bc2233a38b7918b68
ole-na <82842056+ole-na@users.noreply.github.com>
2021-06-27 20:51:27 +0200

    removed unused useEffect

--- frontend/src/components/map/MapContainerComponent.js
+++ frontend/src/components/map/MapContainerComponent.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import "leaflet/dist/leaflet.css";
 import {MapContainer, LayersControl} from 'react-leaflet';
 import {NmScale} from "@marfle/react-leaflet-nmscale";
