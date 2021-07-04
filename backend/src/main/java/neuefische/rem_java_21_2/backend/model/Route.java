98d0228ad978f6f316e33688f39a1617e44a3d65
ole-na <82842056+ole-na@users.noreply.github.com>
2021-07-04 22:21:14 +0200

    add model for route

---
+++ backend/src/main/java/neuefische/rem_java_21_2/backend/model/Route.java
@@ -0,0 +1,21 @@
+package neuefische.rem_java_21_2.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.util.Pair;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection="settings")
+public class Route {
+    private Pair<Double, Double> start;
+    private Pair<Double, Double> end;
+    private ArrayList<Pair<Double, Double>> points;
+}
