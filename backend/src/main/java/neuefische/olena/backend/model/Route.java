e71429dc32e1044e2f1712b64a9086992aa60b13
ole-na <82842056+ole-na@users.noreply.github.com>
2021-07-05 09:55:29 +0200

    removed not used import

--- backend/src/main/java/neuefische/olena/backend/model/Route.java
+++ backend/src/main/java/neuefische/olena/backend/model/Route.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.util.Pair;
 
 import java.util.ArrayList;
 
