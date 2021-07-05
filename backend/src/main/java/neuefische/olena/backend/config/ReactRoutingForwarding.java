805d5f212065815143e2c4e8fb1de3529df79860
ole-na <82842056+ole-na@users.noreply.github.com>
2021-07-05 14:16:07 +0200

    add ReactRoutingForwarding

---
+++ backend/src/main/java/neuefische/olena/backend/config/ReactRoutingForwarding.java
@@ -0,0 +1,12 @@
+package neuefische.olena.backend.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactRoutingForwarding {
+    @RequestMapping (value = "/**/{[path:[^\\.]*}")
+    public String forwardToRoutUrl() {
+        return "forward:/";
+    }
+}
