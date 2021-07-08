3e6e0cc9a3db845dae72129d476dd17dc6877fe5
ole-na <82842056+ole-na@users.noreply.github.com>
2021-07-08 12:43:21 +0200

    removed unused import of useState

--- frontend/src/components/settings/SuccessDialog.js
+++ frontend/src/components/settings/SuccessDialog.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
