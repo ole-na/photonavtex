64b9ca96fbba6b9e2c6b7e688a85f9672057a960
ole-na <82842056+ole-na@users.noreply.github.com>
2021-07-11 13:40:12 +0200

    display route with saved settings data from repository

--- frontend/src/components/map/children/RouteComponent.js
+++ frontend/src/components/map/children/RouteComponent.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Polyline} from "react-leaflet";
+import Loading from "../../Loading";
 
-// TODO: Dummy data, MongoDB data should be used
-const routeCoords = [
+// Dummy data for route testing, now not used
+const dummyRouteCoords = [
     [54.083, 11.070],
     [54.099, 11.226],
     [54.109, 11.427],
@@ -19,5 +20,31 @@ const routeCoords = [
 const colorOptions = { color: 'lime', weight: 5 };
 
 export default function RouteComponent(props) {
-    return <Polyline pathOptions={colorOptions} positions={routeCoords} />
+    const [isRouteLoading, setIsRouteLoading] = useState(false)
+    const [routeCoords, setRouteCoords] = useState([])
+    const [displayRoute, setDisplayRoute] = useState(false)
+
+    useEffect(() => {
+        setIsRouteLoading(true);
+        const startRoute = props.settings.route.start
+        const endRoute = props.settings.route.end
+        const routeCoordsArray = []
+        routeCoordsArray.push(startRoute)
+        routeCoordsArray.push(endRoute)
+        props.settings.route.points?.map((point) => {
+            routeCoordsArray.push(point)
+        })
+
+        setRouteCoords(routeCoordsArray)
+        if(routeCoordsArray.length > 0) {
+
+            setDisplayRoute(true)
+        }
+        setIsRouteLoading(false);
+    }, []);
+
+    return (<>
+        {isRouteLoading && <Loading isLoading={isRouteLoading}/>}
+        {!isRouteLoading && displayRoute && <Polyline pathOptions={colorOptions} positions={routeCoords}/>}
+    </>)
 }
