744724d77b22c2c4c6d8b97a416b1c641f1ce21e
ole-na <82842056+ole-na@users.noreply.github.com>
2021-07-11 16:37:28 +0200

    set center of map to current position or route start

--- frontend/src/components/map/MapContainerComponent.js
+++ frontend/src/components/map/MapContainerComponent.js
@@ -25,6 +25,7 @@ export default function  MapContainerComponent() {
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState(false)
     const [mapSettingsHint, setMapSettingsHint] = useState(false)
+    const [center, setCenter] = useState(mapOptions.center)
 
     const getSettingsFromRepository = () => {
         setIsLoading(true);
@@ -38,14 +39,36 @@ export default function  MapContainerComponent() {
                     return
                 }
                 setSettings(settingsResponse)
-                setIsLoading(false);
-                setError(false);
+                const routeStartLatLong = settingsResponse.route.start
+
+                // locate map to current position if geolocation possible or to route start
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        const currentLatLong = [position.coords.latitude, position.coords.longitude]
+                        setCenter(currentLatLong)
+                        setIsLoading(false);
+                        setError(false);
+                    }, function() {
+                        setCenterToRouteStart(routeStartLatLong);
+                    });
+                } else {
+                    setCenterToRouteStart(routeStartLatLong);
+                }
             })
             .catch((error) => {
                 console.error(error.message)
                 setIsLoading(false);
                 setError(true);
             });
+    }
+
+    function setCenterToRouteStart(routeStartLatLong) {
+        const routePosition = routeStartLatLong
+        if(routePosition.length == 2) {
+            setCenter(routePosition)
+        }
+        setIsLoading(false);
+        setError(false);
     }
 
     useEffect(() => {
@@ -64,7 +87,7 @@ export default function  MapContainerComponent() {
 
                     {settings &&
                         <div id="map">
-                            <MapContainer center={mapOptions.center}
+                            <MapContainer center={center}
                                           zoom={mapOptions.zoom}
                                           scrollWheelZoom={mapOptions.scrollWheelZoom}
                                           style={styles.targetDiv} fadeAnimation={true}
